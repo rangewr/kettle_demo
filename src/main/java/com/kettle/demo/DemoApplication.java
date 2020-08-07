4feac1e5f470eb4b75882dd0f540ac4d8460e463
rangewr <759922853@qq.com>
2020-08-07 16:35:18 +0800

    修改注释

--- src/main/java/com/kettle/demo/DemoApplication.java
+++ src/main/java/com/kettle/demo/DemoApplication.java
@@ -2,7 +2,7 @@
  * @Author: wangran
  * @Date: 2020-06-04 11:50:40
  * @LastEditors: wangran
- * @LastEditTime: 2020-06-05 09:12:07
+ * @LastEditTime: 2020-08-07 16:31:08
  */ 
 package com.kettle.demo;
 
